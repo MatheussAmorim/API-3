3297a21e5961c82e09c3280ff53adacd68ba204d
Matheussamorim <matheus.amorim02@gmail.com>
2023-09-10 17:41:46 -0300

    conexão com banco

--- grails-app/controllers/api3/UrlMappings.groovy
+++ grails-app/controllers/api3/UrlMappings.groovy
@@ -2,16 +2,8 @@ package api3
 
 class UrlMappings {
 
-    static mappings = {
-        delete "/$controller/$id(.$format)?"(action:"delete")
-        get "/$controller(.$format)?"(action:"index")
-        get "/$controller/$id(.$format)?"(action:"show")
-        post "/$controller(.$format)?"(action:"save")
-        put "/$controller/$id(.$format)?"(action:"update")
-        patch "/$controller/$id(.$format)?"(action:"patch")
-
-        "/"(controller: 'application', action:'index')
-        "500"(view: '/error')
-        "404"(view: '/notFound')
+    class UrlMappings {
+        static mappings = {
+            "/$controller/$action?/$id?"()
+        }
     }
-}
